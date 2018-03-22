2f487426bdf6fce7ac5bc400d057aa5120bff985
gaboe <gabrielecegi@gmail.com>
2018-03-22 21:23:06 +0100

    Add some slide

--- presentation/index.js
+++ presentation/index.js
@@ -39,7 +39,7 @@ export default class Presentation extends React.Component {
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Gabo a .NET
+            Gabo &amp; .NET
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
             Nakúkanie do kultúry .NETu na objednávku slavnoj FAI
@@ -55,7 +55,7 @@ export default class Presentation extends React.Component {
                 <ListItem>~2 roky vývoja v .NET</ListItem>
                 <ListItem>BLogic</ListItem>
                 <ListItem>BSP</ListItem>
-                <ListItem>ALO, Colorlak, SummitTrade, Continental, Gumex</ListItem>
+                <ListItem>ALOve, Colorlak, SummitTrade, Continental, Gumex</ListItem>
               </List>
             </Col>
           </Row>
@@ -68,7 +68,23 @@ export default class Presentation extends React.Component {
             <ListItem>Typescript</ListItem>
           </List>
         </Slide>
-
+        <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
+          <Heading size={6} textColor="secondary" caps>DB</Heading>
+          <List>
+            <ListItem>MSSQL</ListItem>
+            <ListItem>Database First</ListItem>
+            <ListItem>Local DB</ListItem>
+            <ListItem>DB Project</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={6} textColor="tertiary" caps>App</Heading>
+          <List>
+            <ListItem>ASP MVC 5</ListItem>
+            <ListItem>Entity Framework</ListItem>
+            <ListItem>DI - Autofac</ListItem>
+          </List>
+        </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
             <Quote>Example Quote</Quote>
